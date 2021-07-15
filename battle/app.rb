a329aa2fff1e1d2c40d246fe08a6a1f347af8aaa
jamescondon <jecondon66@icloud.com>
2021-07-15 13:19:41 +0100

    Passing tests for players to enter their names

--- battle/app.rb
+++ battle/app.rb
@@ -6,10 +6,19 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/' do
-    'Testing infrastructure working!'
-  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+  
 end
