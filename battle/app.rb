d3346be2e69a1e0ec37654c2361903a2f6ff7165
jamescondon <jecondon66@icloud.com>
2021-07-15 13:59:58 +0100

    Employ session and POST-redirect loop

--- battle/app.rb
+++ battle/app.rb
@@ -2,10 +2,10 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
@@ -15,10 +15,15 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
-  
 end
