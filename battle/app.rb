cce436a06cbcb822f6d813d087c3cc4f840305e6
jamescondon <jecondon66@icloud.com>
2021-07-15 23:03:06 +0100

    Extracting Logic to the Model

--- battle/app.rb
+++ battle/app.rb
@@ -8,17 +8,14 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+  post '/names' do
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    redirect '/play'
+  end
 
   get '/' do
     erb :index
-  end
-
-  post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    redirect '/play'
   end
 
   get '/play' do
@@ -28,4 +25,8 @@ class Battle < Sinatra::Base
   get '/attack' do
     erb :attack
   end
+
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
